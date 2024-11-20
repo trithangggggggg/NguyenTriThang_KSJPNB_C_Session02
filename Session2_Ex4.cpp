76b88a8029556472f9dfda69f5d27e9bc73016fd
trithangggggggg <trithang2006vn@gmail.com>
2024-11-21 00:16:19 +0700

    Upload bai tap session 2 - ex4

---
+++ Session2_Ex4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float canh = 10.0;
+    float chu_vi = 4 * canh;
+    float dien_tich = canh * canh;
+    
+    printf("Canh hinh vuong: %.2f\n", canh);
+    printf("Chu vi hinh vuong: %.2f\n", chu_vi);
+    printf("Dien tich hinh vuong: %.2f\n", dien_tich);
+
+    return 0;
+}
