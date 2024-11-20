af175a935326ef305f0180d324469e97487e2e26
trithangggggggg <trithang2006vn@gmail.com>
2024-11-20 11:44:08 +0700

    upload bai tap session 2 ex1

---
+++ Session2_Ex1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	int a = 2, b = 3; // khai bao bien
+	float result;	// kieu so thuc
+	result =  (float)a + b; // toan tu cong
+	printf("tong hai so la %.1f\n",result);
+	return 0;
+}
