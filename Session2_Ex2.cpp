dd5ee1c0d89905e3ab89028579a2313c84ea0026
trithangggggggg <trithang2006vn@gmail.com>
2024-11-20 14:09:54 +0700

    Upload bai tap session 2 - ex2

---
+++ Session2_Ex2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2, b = 3;
+	float result = (float)a + b;
+	printf("so nguyen: a = %d, b = %d\n", a, b);
+	printf("so thuc: result = %.2f\n", result);
+	return 0;
+}
