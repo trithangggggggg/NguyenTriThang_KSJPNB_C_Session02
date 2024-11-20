8c1bb30ab70a97b4561e39ce41591cb67e59b30c
trithangggggggg <trithang2006vn@gmail.com>
2024-11-21 00:11:22 +0700

    Upload bai tap session 2 - Ex3

---
+++ Session2_Ex3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int a = 15;
+	int b = 5;
+	
+	int tong = a + b;
+	printf("phep cong la %d\n",tong);
+	 
+	int hieu = a - b;
+	printf("phep tru la %d\n",hieu);
+	 
+	int tich = a * b;
+	printf("phep nhan là %d\n",tich);
+	
+	int thuong = a  / b;
+	printf("phep chia la %d\n",thuong); 
+	 
+	return 0; 
+} 
